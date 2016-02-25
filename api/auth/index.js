b0392648c6e36fc65528b462769ee02d73653b7e
Loc Nguyen <lochnguyen@gmail.com>
2016-02-25 08:18:47 -0800

    #12: Add proxying endpoints for Meetup's Oauth2 provider

--- api/auth/index.js
+++ api/auth/index.js
@@ -7,8 +7,22 @@ const jwt = require('jsonwebtoken');
 
 
 function register (server, options, next) {
-
     const meetup = server.settings.app.meetup;
+
+    function getOauthParams(options) {
+        return `client_id=${meetup.oauthClientId}&client_secret=${meetup.oauthClientSecret}&grant_type=${options.grant_type}&redirect_uri=http%3A%2F%2Flocalhost%3A8080`;
+    }
+
+    server.route({
+        method: 'GET',
+        path: '/oauth2/authorize',
+        config: {
+            handler(request, reply) {
+                let url = `https:\/\/secure.meetup.com/oauth2/authorize?client_id=${meetup.oauthClientId}&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A8080`;
+                reply.redirect(url);
+            }
+        }
+    });
 
     server.route({
         method: 'POST',
@@ -19,8 +33,8 @@ function register (server, options, next) {
                     code: Joi.string().required()
                 }
             },
-            handler: function (request, reply) {
-                let queryParams = `client_id=${meetup.oauthClientId}&client_secret=${meetup.oauthClientSecret}&grant_type=authorization_code&redirect_uri=http%3A%2F%2Fmeetarang.io%3A8080&code=${request.query.code}`;
+            handler(request, reply) {
+                let queryParams = `${getOauthParams({grant_type: 'authorization_code'})}&code=${request.query.code}`;
 
                 let config = {
                     headers: {
@@ -40,13 +54,51 @@ function register (server, options, next) {
                         let json = JSON.parse(payload);
                         let token = jwt.sign(json, server.settings.app.jwtKey, {algorithm: 'HS256'});
                         reply({token: token});
-                    
+
                     } else {
-                        reply(Boom.create(response.statusCode, 'Meetup could not complete authentication'));
+                        console.log(JSON.parse(payload));
+                        reply(Boom.create(response.statusCode, 'Meetup could not complete authentication', payload));
                     }
                 });
             }
-        },
+        }
+    });
+
+    server.route({
+        method: 'POST',
+        path: '/oauth2/refresh',
+        config: {
+            auth: 'token',
+            handler(request, reply) {
+                let queryParams = `${getOauthParams({grant_type: 'refresh_token'})}&refresh_token=${request.auth.credentials.refresh_token}`;
+
+                let config = {
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded',
+                        'Accept': 'application/json'
+                    },
+                    payload: queryParams
+                };
+
+                Wreck.post(`${server.settings.app.meetup.apiBase}/oauth2/access`, config, (err, response, payload) => {
+                    if (err) {
+                        reply(Boom.badImplementation('Server error, could not complete authentication'));
+                    }
+
+                    if (response.statusCode === 200) {
+
+                        let json = JSON.parse(payload);
+                        console.log(json);
+                        let token = jwt.sign(json, server.settings.app.jwtKey, {algorithm: 'HS256'});
+                        reply({token: token});
+
+                    } else {
+                        console.log(JSON.parse(payload));
+                        reply(Boom.create(response.statusCode, 'Meetup could not complete authentication', payload));
+                    }
+                });
+            }
+        }
     });
 
     next();
--- api/index.js
+++ api/index.js
@@ -13,9 +13,6 @@ server.connection({ port: config.port });
 
 server.register([
     {
-        register: require('api/auth')
-    },
-    {
         register: require('hapi-auth-jwt')
     }
 ], (err) => {
@@ -26,8 +23,27 @@ server.register([
 
     server.auth.strategy('token', 'jwt', {
         key: config.jwtKey
-    })
+    });
 });
+
+server.register(
+    [
+        {
+            register: require('api/auth')
+        }
+    ],
+    {
+        routes: {
+            prefix: '/proxy'
+        }
+    },
+    (err) => {
+        if (err) {
+            console.log('Could not register all the Hapi plugins');
+            throw err;
+        }
+    }
+);
 
 server.route({
     method: 'GET',
@@ -35,6 +51,7 @@ server.route({
     config: {
         auth: 'token',
         handler: function (request, reply) {
+            console.log(request.auth.credentials);
             reply('hello world');
         }
     }
